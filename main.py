0e96eb223f14544d5e5c3ba544ca7289aeccdf14
Alex <90189167+AlexBeesley@users.noreply.github.com>
2022-12-20 22:02:33 +0000

    upload

--- main.py
+++ main.py
@@ -1,77 +1,89 @@
-import glob
-import os
-import threading
-
 import numpy as np
 import tensorflow as tf
-import wavfile
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-base_path = 'C:\dev\CI642\Music_Genre_Classification\Data\genres_original'
-data = []
-labels = []
+from PreProccessing import LoadData
+
+
+def divide_dataset(Data, Labels):
+    x_train, x_test, y_train, y_test = train_test_split(Data, Labels, test_size=0.2, random_state=42)
+    return x_train, x_test, y_train, y_test
+
+
 print("Initializing...")
 
+data, labels = LoadData.RunProcess()
 
-def process_file(file_path):
-    try:
-        audio_data, sr, br = wavfile.read(file_path)
-        audio_data = (audio_data - np.min(audio_data)) / (np.max(audio_data) - np.min(audio_data))
-        data.append(audio_data)
-        labels.append(item)
-        print(f"Processed file: {file_path}")
-    except:
-        print(f"Unknown error occurred while processing file: {file_path}\nSkipping...")
+le = LabelEncoder()
+le.fit(labels)
+labels_int = le.transform(labels)
+labels_one_hot = tf.one_hot(labels_int, depth=10)
+labels = np.asarray(labels_one_hot)
 
-
-threads = []
-
-for item in os.listdir(base_path):
-    path = os.path.join(base_path, item)
-    print(f"Working on folder: {path}")
-    for file_path in glob.glob(os.path.join(path, '*.wav')):
-        thread = threading.Thread(target=process_file, args=(file_path,))
-        threads.append(thread)
-        thread.start()
-
-for thread in threads:
-    thread.join()
-
-print("Finished processing all files")
-
-data = np.asarray(data)
-labels = np.asarray(labels)
+X_train, X_test, Y_train, Y_test = divide_dataset(data, labels)
 
 
-def divide_dataset(x, y, test_size=0.2):
-    np.random.seed(0)
-    indices = np.random.permutation(len(x))
-    x = x[indices]
-    y = y[indices]
-    num_test_samples = int(len(x) * test_size)
-    x_test = x[:num_test_samples]
-    y_test = y[:num_test_samples]
-    x_train = x[num_test_samples:]
-    y_train = y[num_test_samples:]
-    return x_test, x_train, y_test, y_train
+def ShapeX_Data(X_data):
+    max_shape = max([item.shape for item in X_data], key=lambda x: np.prod(x))
+    padded_data = []
+    print("\tpadding data...")
+    for item in X_data:
+        pad_rows = max_shape[0] - item.shape[0]
+        pad_cols = max_shape[1] - item.shape[1]
+        padded_item = np.pad(item, [(0, pad_rows), (0, pad_cols)], mode='constant')
+        padded_data.append(padded_item)
+    print("\tstacking data...")
+    X_data = np.stack(padded_data, axis=0)
+    print("\tdownsampling...")
+    X_data = np.resize(X_data, (16, 16, 10))
+    print("\tconverting to tensor...")
+    X_data = tf.convert_to_tensor(X_data, dtype=tf.float16)
+    print(np.shape(X_data))
+    return X_data
 
 
+def ShapeY_Data(Y_data):
+    Y_data = np.asarray(Y_data)
+    Y_data = np.stack(Y_data, axis=0)
+    Y_data = np.resize(Y_data, (16, 16, 10))
+    Y_data = tf.convert_to_tensor(Y_data, dtype=tf.float16)
+    return Y_data
+
+
+print("shaping X_train data:")
+X_train = ShapeX_Data(X_train)
+print("shaping X_test data:")
+X_test = ShapeX_Data(X_test)
+print("shaping Y_train data:")
+Y_train = ShapeY_Data(Y_train)
+print("shaping Y_test data:")
+Y_test = ShapeY_Data(Y_test)
+
+print(X_train.shape, Y_train.shape)
+print(X_train[1])
+print(Y_train[1])
+
+print("creating model...")
 model = tf.keras.Sequential([
-    tf.keras.layers.Reshape((6990, 130, 13)),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(16, (2, 2), activation='relu', input_shape=(X_train.shape), dtype=tf.float16),
     tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
+print(model.summary())
+
+print("compiling...")
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-X_test, X_train, Y_test, Y_train = divide_dataset(data, labels)
+print("fitting...")
+model.fit(X_train, Y_train, epochs=20, batch_size=16)
 
-model.fit(X_train, Y_train, epochs=10)
-
+print("evaluating...")
 model.evaluate(X_test, Y_test, verbose=2)
